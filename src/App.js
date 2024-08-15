c28965bda6ae42764b31bfa92534636f065f426c
Mohit kumar <mk513147@gmail.com>
2024-08-15 23:11:02 +0530

    Optimized code and fixed image issue

--- src/App.js
+++ src/App.js
@@ -19,6 +19,31 @@ const newsData = [
     key: "business",
     category: "business"
   },
+  {
+    path: "/entertainment",
+    key: "entertainment",
+    category: "entertainment"
+  },
+  {
+    path: "/health",
+    key: "health",
+    category: "health"
+  },
+  {
+    path: "/science",
+    key: "science",
+    category: "science"
+  },
+  {
+    path: "/sports",
+    key: "sports",
+    category: "sports"
+  },
+  {
+    path: "/technology",
+    key: "technology",
+    category: "technology"
+  },
 ]
 export default class App extends Component {
   pageSize = 15;
@@ -33,14 +58,6 @@ export default class App extends Component {
                 <Route exact path={element.path} element={<News key={element.key} pageSize={this.pageSize} country="in" category={element.category} />} />
               ))
             }
-            {/* <Route exact path='/' element={<News key="general" pageSize={this.pageSize} country="in" category='general' />} /> */}
-
-            <Route exact path='/business' element={<News key="business" pageSize={this.pageSize} country="in" category='business' />} />
-            <Route exact path='/entertainment' element={<News key="entertainment" pageSize={this.pageSize} country="in" category='entertainment' />} />
-            <Route exact path='/health' element={<News key="health" pageSize={this.pageSize} country="in" category='health' />} />
-            <Route exact path='/science' element={<News key="science" pageSize={this.pageSize} country="in" category='science' />} />
-            <Route exact path='/sports' element={<News key="sports" pageSize={this.pageSize} country="in" category='sports' />} />
-            <Route exact path='/technology' element={<News key="technology" pageSize={this.pageSize} country="in" category='technology' />} />
           </Routes>
           <NewsDetail />
         </BrowserRouter>
