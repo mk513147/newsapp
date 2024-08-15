3a30d965270cb20a4eb31100d055a25f601ee877
kaushal <kausraj11@gmail.com>
2024-08-15 15:31:07 +0530

    FIXED

--- src/Components/News.js
+++ src/Components/News.js
@@ -5,13 +5,13 @@ import PropTypes from 'prop-types'
 
 
 export class News extends Component {
-    static defaultProps ={
+    static defaultProps = {
         country: 'in',
         pageSize: 8,
         category: 'general'
     }
 
-    static propTypes ={
+    static propTypes = {
         country: PropTypes.string,
         pageSize: PropTypes.number,
         category: PropTypes.string,
@@ -26,29 +26,30 @@ export class News extends Component {
         };
     }
 
-    
+
     async updateNews() {
         const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=758fb9d28589446b966ac18e564e47b6&page=${this.state.page}&pageSize=${this.props.pageSize}`;
-        this.setState({loading: true})
+        this.setState({ loading: true })
         let data = await fetch(url);
         let parsedData = await data.json();
         this.setState({
             article: parsedData.articles,
             totalResults: parsedData.totalResults,
             loading: false,
+
         })
     }
-    
+
     async componentDidMount() {
         this.updateNews();
     }
     handlePrev = () => {
-        this.setState({page: this.state.page - 1});
+        this.setState({ page: this.state.page - 1 });
         this.updateNews();
     }
 
     handleNext = () => {
-        this.setState({page: this.state.page + 1});
+        this.setState({ page: this.state.page + 1 });
         this.updateNews();
     }
 
@@ -61,8 +62,8 @@ export class News extends Component {
                     <div className="row">
                         {!this.state.loading && this.state.article.map((element) => {
                             return <div className="col-md-4" key={element.url}>
-                                <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.image_url ? element.urlToImage : ""} 
-                                newsUrl={element.url ? element.url : ""} author={element.author} date={element.publishedAt} source= {element.source.name} />
+                                <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.image_url ? element.urlToImage : ""}
+                                    newsUrl={element.url ? element.url : ""} author={element.author} date={element.publishedAt} source={element.source.name} />
                             </div>
                         })}
                     </div>
